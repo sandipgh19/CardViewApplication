dfdfe265ae4e476159a26e627186d2868b80bcc9
sandipghosh <sandipgh19@gmail.com>
2017-03-21 09:06:55 +0530

    Code

---
+++ app/src/main/java/com/example/sandipghosh/cardviewapplication/Config.java
@@ -0,0 +1,28 @@
+package com.example.sandipghosh.cardviewapplication;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by sandipghosh on 21/03/17.
+ */
+
+public class Config {
+
+    private static String name[];
+    private static String text2[];
+    private static Bitmap bitmap[];
+
+    public static final String URL ="";
+    public static final String imageURL = "";
+    public static final String text1URL = "";
+    public static final String text2URL = "";
+
+    public Config( int i) {
+
+        name = new String[i];
+        text2 = new String[i];
+        bitmap = new Bitmap[i];
+
+    }
+
+}
--- app/src/main/java/com/example/sandipghosh/cardviewapplication/MainActivity.java
+++ app/src/main/java/com/example/sandipghosh/cardviewapplication/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.sandipghosh.cardviewapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    private Config config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycle);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        getData();
+    }
+
+    private void getData() {
+
+        
     }
 }
